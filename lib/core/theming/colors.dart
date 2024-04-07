8ec4375e9234c019b352c8cf5282dd7aa4bcc961
samaa <samaasoltan6@gmail.com>
2024-04-07 04:40:52 +0200

    - PR comments - Fix- Colors naming - Fix- Use colors from it's class

--- lib/core/theming/colors.dart
+++ lib/core/theming/colors.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primaryWhiteColor  = Color(0xFFFFFFFF);
-  static const Color primaryBlackColor  = Color(0xFF000000);
-  static const Color primaryECECECColor = Color(0xFFECECEC);
-  static const Color primaryCBCBCBColor = Color(0xFFCBCBCB);
-  static const Color primaryF5F5F5Color = Color(0xFFF5F5F5);
+  static const Color primaryWhiteColor       = Color(0xFFFFFFFF);
+  static const Color primaryBlackColor       = Color(0xFF000000);
+  static const Color primaryLightGreyColor   = Color(0xFFECECEC);
+  static const Color primaryMediumGrayColor  = Color(0xFFCBCBCB);
+  static const Color primaryLighterGreyColor = Color(0xFFF5F5F5);
 }
--- lib/core/theming/text_styles.dart
+++ lib/core/theming/text_styles.dart
@@ -2,36 +2,35 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../helpers/constants.dart';
-
-
+import 'colors.dart';
 
 class AppTextStyles {
   /// White Fonts
-  static TextStyle fontWhite13W400   = TextStyle(color: Colors.white, fontSize: 13.sp, fontWeight: FontWeight.w400, fontFamily: poppinsFontFamily);
-  static TextStyle fontWhite15W500   = TextStyle(color: Colors.white, fontSize: 15.sp, fontWeight: FontWeight.w500, fontFamily: poppinsFontFamily);
-  static TextStyle fontWhite16W600   = TextStyle(color: Colors.white, fontSize: 16.sp, fontWeight: FontWeight.w600, fontFamily: poppinsFontFamily);
-  static TextStyle fontWhite17W500   = TextStyle(color: Colors.white, fontSize: 17.sp, fontWeight: FontWeight.w500, fontFamily: poppinsFontFamily);
-  static TextStyle fontWhite17W600   = TextStyle(color: Colors.white, fontSize: 17.sp, fontWeight: FontWeight.w600, fontFamily: poppinsFontFamily);
-  static TextStyle fontWhite18W500   = TextStyle(color: Colors.white, fontSize: 18.sp, fontWeight: FontWeight.w500, fontFamily: poppinsFontFamily);
-  static TextStyle fontWhite19W500   = TextStyle(color: Colors.white, fontSize: 19.sp, fontWeight: FontWeight.w500, fontFamily: poppinsFontFamily);
-  static TextStyle fontWhite30W500   = TextStyle(color: Colors.white, fontSize: 30.sp, fontWeight: FontWeight.w500, fontFamily: poppinsFontFamily);
-  static TextStyle fontWhite33W600   = TextStyle(color: Colors.white, fontSize: 33.sp, fontWeight: FontWeight.w600, fontFamily: poppinsFontFamily);
-  static TextStyle fontWhite40W500   = TextStyle(color: Colors.white, fontSize: 40.sp, fontWeight: FontWeight.w500, fontFamily: poppinsFontFamily);
-  static TextStyle fontWhite50W500   = TextStyle(color: Colors.white, fontSize: 50.sp, fontWeight: FontWeight.w500, fontFamily: poppinsFontFamily);
-  static TextStyle fontWhite63W600   = TextStyle(color: Colors.white, fontSize: 63.sp, fontWeight: FontWeight.w600, fontFamily: poppinsFontFamily);
-  static TextStyle fontWhite77W600   = TextStyle(color: Colors.white, fontSize: 77.sp, fontWeight: FontWeight.w600, fontFamily: poppinsFontFamily);
+  static TextStyle fontWhite13W400   = TextStyle(color: AppColors.primaryWhiteColor, fontSize: 13.sp, fontWeight: FontWeight.w400, fontFamily: poppinsFontFamily);
+  static TextStyle fontWhite15W500   = TextStyle(color: AppColors.primaryWhiteColor, fontSize: 15.sp, fontWeight: FontWeight.w500, fontFamily: poppinsFontFamily);
+  static TextStyle fontWhite16W600   = TextStyle(color: AppColors.primaryWhiteColor, fontSize: 16.sp, fontWeight: FontWeight.w600, fontFamily: poppinsFontFamily);
+  static TextStyle fontWhite17W500   = TextStyle(color: AppColors.primaryWhiteColor, fontSize: 17.sp, fontWeight: FontWeight.w500, fontFamily: poppinsFontFamily);
+  static TextStyle fontWhite17W600   = TextStyle(color: AppColors.primaryWhiteColor, fontSize: 17.sp, fontWeight: FontWeight.w600, fontFamily: poppinsFontFamily);
+  static TextStyle fontWhite18W500   = TextStyle(color: AppColors.primaryWhiteColor, fontSize: 18.sp, fontWeight: FontWeight.w500, fontFamily: poppinsFontFamily);
+  static TextStyle fontWhite19W500   = TextStyle(color: AppColors.primaryWhiteColor, fontSize: 19.sp, fontWeight: FontWeight.w500, fontFamily: poppinsFontFamily);
+  static TextStyle fontWhite30W500   = TextStyle(color: AppColors.primaryWhiteColor, fontSize: 30.sp, fontWeight: FontWeight.w500, fontFamily: poppinsFontFamily);
+  static TextStyle fontWhite33W600   = TextStyle(color: AppColors.primaryWhiteColor, fontSize: 33.sp, fontWeight: FontWeight.w600, fontFamily: poppinsFontFamily);
+  static TextStyle fontWhite40W500   = TextStyle(color: AppColors.primaryWhiteColor, fontSize: 40.sp, fontWeight: FontWeight.w500, fontFamily: poppinsFontFamily);
+  static TextStyle fontWhite50W500   = TextStyle(color: AppColors.primaryWhiteColor, fontSize: 50.sp, fontWeight: FontWeight.w500, fontFamily: poppinsFontFamily);
+  static TextStyle fontWhite63W600   = TextStyle(color: AppColors.primaryWhiteColor, fontSize: 63.sp, fontWeight: FontWeight.w600, fontFamily: poppinsFontFamily);
+  static TextStyle fontWhite77W600   = TextStyle(color: AppColors.primaryWhiteColor, fontSize: 77.sp, fontWeight: FontWeight.w600, fontFamily: poppinsFontFamily);
 
-  /// Different Colors Fonts
-  static TextStyle fontF5F5F523W5600 = TextStyle(color: const Color(0xFFF5F5F5), fontSize: 23.sp, fontWeight: FontWeight.w600, fontFamily: poppinsFontFamily);
-  static TextStyle fontCBCBCB15W500  = TextStyle(color: const Color(0xFFCBCBCB), fontSize: 15.sp, fontWeight: FontWeight.w500, fontFamily: poppinsFontFamily);
-  static TextStyle fontCBCBCB16W500  = TextStyle(color: const Color(0xFFCBCBCB), fontSize: 16.sp, fontWeight: FontWeight.w500, fontFamily: poppinsFontFamily);
-  static TextStyle fontECECEC15W500  = TextStyle(color: const Color(0xFFECECEC), fontSize: 15.sp, fontWeight: FontWeight.w500, fontFamily: poppinsFontFamily);
-  static TextStyle fontECECEC18W600  = TextStyle(color: const Color(0xFFECECEC), fontSize: 18.sp, fontWeight: FontWeight.w600, fontFamily: poppinsFontFamily);
-  static TextStyle fontECECEC19W700  = TextStyle(color: const Color(0xFFECECEC), fontSize: 19.sp, fontWeight: FontWeight.w700, fontFamily: poppinsFontFamily);
-  static TextStyle fontECECEC30W400  = TextStyle(color: const Color(0xFFECECEC), fontSize: 30.sp, fontWeight: FontWeight.w400, fontFamily: poppinsFontFamily);
-  static TextStyle fontECECEC30W700  = TextStyle(color: const Color(0xFFECECEC), fontSize: 30.sp, fontWeight: FontWeight.w700, fontFamily: poppinsFontFamily);
+  /// Grey Fonts
+  static TextStyle fontLighterGray23W600 = TextStyle(color: AppColors.primaryLighterGreyColor, fontSize: 23.sp, fontWeight: FontWeight.w600, fontFamily: poppinsFontFamily);
+  static TextStyle fontMediumGrey15W500  = TextStyle(color: AppColors.primaryMediumGrayColor, fontSize: 15.sp, fontWeight: FontWeight.w500, fontFamily: poppinsFontFamily);
+  static TextStyle fontMediumGrey16W500  = TextStyle(color: AppColors.primaryMediumGrayColor, fontSize: 16.sp, fontWeight: FontWeight.w500, fontFamily: poppinsFontFamily);
+  static TextStyle fontLightGrey15W500   = TextStyle(color: AppColors.primaryLightGreyColor, fontSize: 15.sp, fontWeight: FontWeight.w500, fontFamily: poppinsFontFamily);
+  static TextStyle fontLightGrey18W600   = TextStyle(color: AppColors.primaryLightGreyColor, fontSize: 18.sp, fontWeight: FontWeight.w600, fontFamily: poppinsFontFamily);
+  static TextStyle fontLightGrey19W700   = TextStyle(color: AppColors.primaryLightGreyColor, fontSize: 19.sp, fontWeight: FontWeight.w700, fontFamily: poppinsFontFamily);
+  static TextStyle fontLightGrey30W400   = TextStyle(color: AppColors.primaryLightGreyColor, fontSize: 30.sp, fontWeight: FontWeight.w400, fontFamily: poppinsFontFamily);
+  static TextStyle fontLightGrey30W700   = TextStyle(color: AppColors.primaryLightGreyColor, fontSize: 30.sp, fontWeight: FontWeight.w700, fontFamily: poppinsFontFamily);
 
   /// Black Fonts
-  static TextStyle fontBlack21W500   = TextStyle(color: Colors.black, fontSize: 21.sp, fontWeight: FontWeight.w500, fontFamily: poppinsFontFamily);
+  static TextStyle fontBlack21W500 = TextStyle(color: AppColors.primaryBlackColor, fontSize: 21.sp, fontWeight: FontWeight.w500, fontFamily: poppinsFontFamily);
 
 }
