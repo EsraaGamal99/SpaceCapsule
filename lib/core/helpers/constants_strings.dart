7da8260a622c357f84649869cef202dcfb6caf3d
samaa <samaasoltan6@gmail.com>
2024-04-07 02:40:55 +0200

    - Add text styles, assets, colors, strings and constants

---
+++ lib/core/helpers/constants_strings.dart
@@ -0,0 +1,54 @@
+// -------------------- OnBoarding Strings -------------------------
+const String virtualTextKey                   = 'Virtual';
+const String galaxyTextKey                    = 'Galaxy';
+const String explorerTextKey                  = 'Explorer';
+const String weWillLearnTextKey               = 'We will learn all of the scientific disciplines that involve.';
+const String skipTextKey                      = 'Skip';
+const String startTextKey                     = 'Start';
+const String insightsTextKey                  = 'Insights';
+const String embarkOnAnInterstellarTextKey    = 'Embark on an interstellar voyage like  never before. Explore distant galaxies,  uncover cosmic secrets, and witness the majesty of the universe. Your cosmic  adventure begins here.';
+const String learnAboutTextKey                = 'Learn about';
+const String rocketsTextKey                   = 'Rockets';
+const String exploreTheMarvelsTextKey         = 'Explore the Marvels of Galaxies: Dive into a cosmic journey to learn about the awe-inspiring galaxies that populate our universe.';
+const String dragonsTextKey                   = 'Dragons';
+const String stellarDiscoveriesTextKey        = 'Stellar Discoveries: Embark on an illuminating voyage to uncover the secrets of stars. From the blazing birth of stars to the enchanting constellations that adorn our night sky.';
+const String landpadsTextKey                  = 'Landpads';
+const String ventureIntoTextKey               = 'Venture into the realm of man-made wonders that orbit our planet. In this section, you\'ll uncover the ingenuity behind satellites, from communication satellites linking the world to Earth.';
+const String letsFloatInTextKey               = 'Let’s float in';
+const String universeTextKey                  = 'Universe';
+const String welcomeTextKey                   = 'Welcome';
+const String unlockAUniverseTextKey           = 'Unlock a universe of knowledge and adventure with your pass.';
+const String createNewAccountTextKey          = 'Create new account';
+
+// -------------------- Login Strings -------------------------
+const String backTextKey                       = 'Back';
+const String emailAddressTextKey               = 'Email Address';
+const String passwordTextKey                   = 'Password';
+const String logInTextKey                      = 'Log In';
+const String forgotPasswordTextKey             = 'Forgot Password?';
+const String yourNameTextKey                   = 'Your Name';
+const String signUpTextKey                     = 'Sign Up';
+const String welcomeOnboardTextKey             = 'Welcome Onboard,';
+const String spaceVoyagerTextKey               = 'Space Voyager!';
+
+// -------------------- Home Strings -------------------------
+const String searchYourFavoritePlaceTextKey    = 'Search your favorite place...';
+const String topPicksTextKey                   = 'Top picks';
+const String letsExploreOurProgramsTextKey     = 'Let’s explore our programs';
+const String adventurerTextKey                 = 'Adventurer';
+const String insightfulTextKey                 = 'Insightful';
+const String cheeringTextKey                   = 'Cheering';
+
+// -------------------- Profile Strings -------------------------
+const String appTextKey                        = 'App';
+const String settingsTextKey                   = 'Settings';
+const String editProfileTextKey                = 'Edit profile';
+const String appPreferencesTextKey             = 'App Preferences';
+const String appNotificationsTextKey           = 'App Notifications';
+const String helpAndSupportTextKey             = 'Help and Support';
+const String giveFeedbackTextKey               = 'Give a feedback';
+const String editTextKey                       = 'Edit';
+const String profileTextKey                    = 'Profile';
+const String userNameTextKey                   = 'Username';
+const String emailTextKey                      = 'Email';
+
---
+++ lib/core/theming/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryWhiteColor  = Color(0xFFFFFFFF);
+  static const Color primaryBlackColor  = Color(0xFF000000);
+  static const Color primaryECECECColor = Color(0xFFECECEC);
+  static const Color primaryCBCBCBColor = Color(0xFFCBCBCB);
+  static const Color primaryF5F5F5Color = Color(0xFFF5F5F5);
+}
