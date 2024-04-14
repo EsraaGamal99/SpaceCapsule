32ef3388138c73b9fe4e92657a59da775b654da0
earaagamal99 <esraa66m@gmail.com>
2024-04-14 04:45:37 +0200

    integrate with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:space_app/core/routing/app_router.dart';
 import 'package:space_app/space_app.dart';
 
-void main() {
+import 'core/helpers/bloc_observer.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  Bloc.observer = MyBlocObserver();
+
   runApp(SpaceApp(appRouter: AppRouter()));
 }
