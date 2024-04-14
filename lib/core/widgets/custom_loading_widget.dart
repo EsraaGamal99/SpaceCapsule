7a2adb73627cde33e4aa5f0aa031b4b60fe688a7
earaagamal99 <esraa66m@gmail.com>
2024-04-14 04:55:22 +0200

    remove unused comment from custom_loading_widget.dart

--- lib/core/widgets/custom_loading_widget.dart
+++ lib/core/widgets/custom_loading_widget.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:space_app/core/theming/colors.dart';
 
 class CustomLoadingWidget extends StatelessWidget {
   const CustomLoadingWidget({super.key});
@@ -7,9 +6,7 @@ class CustomLoadingWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Center(
-      child: CircularProgressIndicator(
-        //color: AppColors.primary,
-      ),
+      child: CircularProgressIndicator(),
     );
   }
 }
