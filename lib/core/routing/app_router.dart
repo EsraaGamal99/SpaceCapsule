e34be243f633fe82be9c86cd075963b35894b14e
earaagamal99 <esraa66m@gmail.com>
2024-04-14 04:39:01 +0200

    add login and register routs to app router

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:space_app/core/routing/routes.dart';
+import 'package:space_app/features/authentication/ui/screens/login_screen.dart';
+import 'package:space_app/features/authentication/ui/screens/register_screen.dart';
 
 import '../../features/home/ui/screens/home_screen.dart';
 import '../../features/not_found/not_found_screen.dart';
@@ -9,6 +11,11 @@ class AppRouter {
     switch (routeSettings.name) {
       case Routes.homeScreen:
         return MaterialPageRoute(builder: (context) => const HomeScreen());
+       case Routes.registerScreen:
+        return MaterialPageRoute(builder: (context) => const RegisterScreen());
+       case Routes.loginScreen:
+        return MaterialPageRoute(builder: (context) => const LoginScreen());
+
       default:
         return MaterialPageRoute(builder: (context) => NotFoundScreen(routeSettingsName: routeSettings.name));
     }
--- lib/core/routing/routes.dart
+++ lib/core/routing/routes.dart
@@ -1,3 +1,5 @@
 class Routes {
   static const String homeScreen = '/homeScreen';
+  static const String registerScreen = '/registerScreen';
+  static const String loginScreen = '/loginScreen';
 }
