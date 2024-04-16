2ed3e5c9dfbe40eb1e96c99e15b981a927a3fc4f
samaa <samaasoltan6@gmail.com>
2024-04-16 02:50:26 +0200

    - Add routes for profile and edit profile screens

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:space_app/core/routing/routes.dart';
+import 'package:space_app/features/profile/ui/screens/edit_profile_screen.dart';
+import 'package:space_app/features/profile/ui/screens/profile_screen.dart';
 
 import '../../features/home/ui/screens/home_screen.dart';
 import '../../features/not_found/not_found_screen.dart';
@@ -9,8 +11,14 @@ class AppRouter {
     switch (routeSettings.name) {
       case Routes.homeScreen:
         return MaterialPageRoute(builder: (context) => const HomeScreen());
+      case Routes.profileScreen:
+        return MaterialPageRoute(builder: (context) => const ProfileScreen());
+      case Routes.editProfileScreen:
+        return MaterialPageRoute(builder: (context) => const EditProfileScreen());
       default:
-        return MaterialPageRoute(builder: (context) => NotFoundScreen(routeSettingsName: routeSettings.name));
+        return MaterialPageRoute(
+            builder: (context) =>
+                NotFoundScreen(routeSettingsName: routeSettings.name));
     }
   }
 }
--- lib/core/routing/routes.dart
+++ lib/core/routing/routes.dart
@@ -1,3 +1,5 @@
 class Routes {
   static const String homeScreen = '/homeScreen';
+  static const String profileScreen = '/profileScreen';
+  static const String editProfileScreen = '/editProfileScreen';
 }
