d66e588832a1e8eacd4f1b00b590462bb0363a27
samaa <samaasoltan6@gmail.com>
2024-04-18 00:41:31 +0200

    Add routes and app router for app preferences screen

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:space_app/core/routing/routes.dart';
+import 'package:space_app/features/profile/ui/screens/app_preferences_screen.dart';
 import 'package:space_app/features/profile/ui/screens/edit_profile_screen.dart';
 import 'package:space_app/features/profile/ui/screens/profile_screen.dart';
 
@@ -15,6 +16,8 @@ class AppRouter {
         return MaterialPageRoute(builder: (context) => const ProfileScreen());
       case Routes.editProfileScreen:
         return MaterialPageRoute(builder: (context) => const EditProfileScreen());
+      case Routes.appPreferencesScreen:
+        return MaterialPageRoute(builder: (context) => const AppPreferencesScreen());
       default:
         return MaterialPageRoute(
             builder: (context) =>
