a0804bb6ac5b97198f6ab6c205894a0d4eb196a9
samaa <samaasoltan6@gmail.com>
2024-04-21 01:23:04 +0200

    Add dependency injection, bloc provider and bloc builder to my app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:space_app/core/di/dependency_injection.dart';
 import 'package:space_app/core/routing/app_router.dart';
 import 'package:space_app/space_app.dart';
 
-void main() {
-  runApp(SpaceApp(appRouter: AppRouter()));
+import 'features/localization/logic/localization_cubit.dart';
+
+void main() async {
+  await initGetIt();
+  runApp(BlocProvider(
+      create: (context) => getIt<LocalizationCubit>(),
+      child: SpaceApp(appRouter: AppRouter()),
+  ));
 }
