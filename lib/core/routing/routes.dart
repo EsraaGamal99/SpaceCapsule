0cfdd1e762aaf7c809c29878123a3a5468f17836
samaa <samaasoltan6@gmail.com>
2024-04-25 00:59:45 +0200

    Add routes for onboarding screen

--- lib/core/routing/routes.dart
+++ lib/core/routing/routes.dart
@@ -1,6 +1,10 @@
 class Routes {
-  static const String homeScreen            = '/homeScreen';
-  static const String profileScreen         = '/profileScreen';
-  static const String editProfileScreen     = '/editProfileScreen';
-  static const String appPreferencesScreen  = '/appPreferencesScreen';
+  static const String splashScreen           = '/splashScreen';
+  static const String firstOnboardingScreen  = '/onboardingScreen';
+  static const String secondOnboardingScreen = '/secondOnboardingScreen';
+  static const String welcomeScreen          = '/welcomeScreen';
+  static const String homeScreen             = '/homeScreen';
+  static const String profileScreen          = '/profileScreen';
+  static const String editProfileScreen      = '/editProfileScreen';
+  static const String appPreferencesScreen   = '/appPreferencesScreen';
 }
