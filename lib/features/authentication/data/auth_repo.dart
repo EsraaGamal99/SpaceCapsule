6bdfcbe3df74e73642d201d20fb2a791e4db9ddc
earaagamal99 <esraa66m@gmail.com>
2024-04-26 13:25:42 +0300

    refactor auth repo "without either"

--- lib/features/authentication/data/auth_repo.dart
+++ lib/features/authentication/data/auth_repo.dart
@@ -1,46 +1,52 @@
-import 'package:dartz/dartz.dart';
 import 'package:flutter/foundation.dart';
-import 'package:space_app/core/errors/failure.dart';
-import 'package:space_app/core/helpers/constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:space_app/core/networking/firebase_service/firebase_auth_error_handler/firebase_auth_error-handler.dart';
+import 'package:space_app/core/networking/firebase_service/firebase_result.dart';
+import 'package:space_app/core/networking/general_error_handler.dart';
+
+import 'models/login_model.dart';
+import 'models/register_model.dart';
 
 class AuthRepo {
-
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  Future<Either<Failure, dynamic>> userLogin(
-      {required String email, required String password}) async {
+  Future<FirebaseResult<AuthResultStatus>> logIn(LoginModel loginModel) async {
     try {
-      UserCredential userCredential = await _auth.signInWithEmailAndPassword(
-        email: email,
-        password: password,
+      final UserCredential response = await _auth.signInWithEmailAndPassword(
+        email: loginModel.email,
+        password: loginModel.password,
       );
-
-      token = await userCredential.user!.getIdToken();
-
-      return Right(userCredential);
-    } catch (e) {
-      debugPrint(e.toString());
-      return Left(Failure(errMessage: 'Authentication failed: $e'));
+      if (response.user != null) {
+        debugPrint(
+            '=========================${response.user}=========================');
+        return const FirebaseResult.success(AuthResultStatus.successful);
+      } else {
+        return FirebaseResult.failure(
+            Handler.handle(ErrorHandlerAuth(AuthResultStatus.unknown)));
+      }
+    } catch (error) {
+      return FirebaseResult.failure(Handler.handle(ErrorHandlerAuth(error)));
     }
   }
 
-  Future<Either<Failure, dynamic>> userRegistration(
-      {required String name,
-        required String email,
-        required String password}) async {
+  Future<FirebaseResult<AuthResultStatus>> register(
+      RegisterModel registerModel) async {
     try {
-      UserCredential userCredential =
-      await _auth.createUserWithEmailAndPassword(
-        email: email,
-        password: password,
+      final UserCredential response =
+          await _auth.createUserWithEmailAndPassword(
+        email: registerModel.email,
+        password: registerModel.password,
       );
-
-      return Right(userCredential);
-    } catch (e) {
-      debugPrint(e.toString());
-      return Left(Failure(errMessage: 'Authentication failed: $e'));
+      if (response.user != null) {
+        debugPrint(
+            '=========================${response.user}=========================');
+        return const FirebaseResult.success(AuthResultStatus.successful);
+      } else {
+        return FirebaseResult.failure(
+            Handler.handle(ErrorHandlerAuth(AuthResultStatus.unknown)));
+      }
+    } catch (error) {
+      return FirebaseResult.failure(Handler.handle(ErrorHandlerAuth(error)));
     }
   }
-
 }
