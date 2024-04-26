b4beb1777f9c926216ce4a15d794ec68b53138c1
earaagamal99 <esraa66m@gmail.com>
2024-04-26 13:31:06 +0300

    refactor register body

--- lib/features/authentication/ui/screens/widgets/register_body.dart
+++ lib/features/authentication/ui/screens/widgets/register_body.dart
@@ -1,108 +1,68 @@
-import 'package:conditional_builder_null_safety/conditional_builder_null_safety.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:space_app/core/helpers/constants_strings.dart';
-import 'package:space_app/core/helpers/functions/show_snack_bar.dart';
-import 'package:space_app/core/helpers/functions/success_register_show_dialog.dart';
-import 'package:space_app/core/routing/routes.dart';
 import 'package:space_app/core/theming/text_styles.dart';
-import 'package:space_app/core/widgets/custom_loading_widget.dart';
 import 'package:space_app/core/widgets/custom_material_button.dart';
 import 'package:space_app/features/authentication/logic/register_cubit/register_cubit.dart';
+import 'package:space_app/features/authentication/ui/screens/widgets/register_bloc_listener.dart';
 import 'package:space_app/features/authentication/ui/screens/widgets/user_data_section.dart';
 
-class RegisterBody extends StatelessWidget {
-  RegisterBody({super.key});
+import 'already_have_account.dart';
 
-  final TextEditingController nameController = TextEditingController();
-  final TextEditingController emailController = TextEditingController();
-  final TextEditingController passwordController = TextEditingController();
+class RegisterBody extends StatelessWidget {
+  const RegisterBody({super.key});
+
 
   @override
   Widget build(BuildContext context) {
-    return BlocConsumer<RegisterCubit, RegisterState>(
-      listener: (context, state) {
-        if (state is RegisterErrorState) {
-          showSnackBar(context, state.errMessage);
-        } else if (state is RegistersSuccessState) {
-          successRegisterShowDialog(context);
-        }
-      },
-      builder: (context, state) {
-        return Padding(
-          padding: const EdgeInsets.all(50.0),
-          child: SingleChildScrollView(
-            child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  SizedBox(
-                    height: 35.h,
-                  ),
-                  Text(
-                    letsTextKey,
-                    style: AppTextStyles.fontWhite40W500,
-                  ),
-                  Text(
-                    startTextKey,
-                    style: AppTextStyles.fontWhite63W600,
-                  ),
-                  UserDataSection(
-                    isLogin: false,
-                    nameController: nameController,
-                    emailController: emailController,
-                    passwordController: passwordController,
-                  ),
-                  ConditionalBuilder(
-                    condition: state is! RegisterLoadingState,
-                    builder: (context) => CustomMaterialButton(
-                      onPressed: () async {
-                        await BlocProvider.of<RegisterCubit>(context)
-                            .userRegistration(
-                          name: nameController.text,
-                          email: emailController.text,
-                          password: passwordController.text,
-                        );
-                      },
-                      label: signUpTextKey,
-                    ),
-                    fallback: (context) => const CustomLoadingWidget(),
-                  ),
-                  SizedBox(
-                    height: 10.h,
-                  ),
-                  Row(
-                    children: [
-                       Expanded(child: Text('Already have an account?',style: AppTextStyles.fontWhite15W500,)),
-                      TextButton(
-                          onPressed: () {
-                            Navigator.pushNamed(context, Routes.loginScreen);
-                          },
-                          child:  Text(
-                            'Login',
-                            style: AppTextStyles.fontWhite17W500,
-                          )
-                      )
-                    ],
-                  ),
-                  SizedBox(
-                    height: 10.h,
-                  ),
-                  Center(
-                    child: TextButton(
-                      onPressed: () {},
-                      child: Text(
-                        termsAndConditionsTextKey,
-                        style: AppTextStyles.fontWhite14W500WithUnderLine,
-                      ),
-                    ),
-                  ),
-                ]),
-          ),
-        );
-      },
+    var read = context.read<RegisterCubit>();
+    return Padding(
+      padding: const EdgeInsets.all(50.0),
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            SizedBox(
+              height: 35.h,
+            ),
+            Text(
+              letsTextKey,
+              style: AppTextStyles.fontWhite40W500,
+            ),
+            Text(
+              startTextKey,
+              style: AppTextStyles.fontWhite63W600,
+            ),
+            UserDataSection(
+              isLogin: false,
+              nameController: read.nameController,
+              emailController: read.emailController,
+              passwordController: read.passwordController,
+            ),
+            CustomMaterialButton(
+              onPressed: () async {
+                await BlocProvider.of<RegisterCubit>(context).userRegistration(
+                  name: read.nameController.text,
+                  email: read.emailController.text,
+                  password: read.passwordController.text,
+                );
+              },
+              label: signUpTextKey,
+            ),
+            SizedBox(
+              height: 10.h,
+            ),
+            const AlreadyHaveAnAccount(),
+            SizedBox(
+              height: 10.h,
+            ),
+            const RegisterBlocListener(),
+          ],
+        ),
+      ),
     );
   }
 }
