e2a9dfefe630d3524cd5d06b93078d1cbe337117
earaagamal99 <esraa66m@gmail.com>
2024-04-26 13:53:44 +0300

    solve conflicts after merge development into authentication "my branch"

--- lib/core/routing/routes.dart
+++ lib/core/routing/routes.dart
@@ -1,7 +1,6 @@
 class Routes {
-  static const String homeScreen = '/homeScreen';
-  static const String registerScreen = '/registerScreen';
-  static const String loginScreen = '/loginScreen';
+  static const String registerScreen        = '/registerScreen';
+  static const String loginScreen           = '/loginScreen';
   static const String homeScreen            = '/homeScreen';
   static const String profileScreen         = '/profileScreen';
   static const String editProfileScreen     = '/editProfileScreen';
