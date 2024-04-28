665a6bee812488dc43d27bb68cbadd9941ed42c4
earaagamal99 <esraa66m@gmail.com>
2024-04-28 19:05:19 +0300

    resolve conflicts

--- lib/core/routing/routes.dart
+++ lib/core/routing/routes.dart
@@ -5,17 +5,8 @@ class Routes {
   static const String profileScreen         = '/profileScreen';
   static const String editProfileScreen     = '/editProfileScreen';
   static const String appPreferencesScreen  = '/appPreferencesScreen';
-  static const String profileScreen          = '/profileScreen';
-  static const String editProfileScreen      = '/editProfileScreen';
-  static const String appPreferencesScreen   = '/appPreferencesScreen';
-  static const String registerScreen         = '/registerScreen';
-  static const String loginScreen            = '/loginScreen';
   static const String splashScreen           = '/splashScreen';
   static const String firstOnboardingScreen  = '/onboardingScreen';
   static const String secondOnboardingScreen = '/secondOnboardingScreen';
   static const String welcomeScreen          = '/welcomeScreen';
-  static const String homeScreen             = '/homeScreen';
-  static const String profileScreen          = '/profileScreen';
-  static const String editProfileScreen      = '/editProfileScreen';
-  static const String appPreferencesScreen   = '/appPreferencesScreen';
 }
