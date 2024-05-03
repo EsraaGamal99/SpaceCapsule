1f0ac7a56c8698ef9b49bb8229f95ee49e3d8184
earaagamal99 <esraa66m@gmail.com>
2024-05-03 12:08:21 +0300

    remove unused imports from rockets_details_screen

--- lib/features/rockets/ui/screens/rockets_details_screen.dart
+++ lib/features/rockets/ui/screens/rockets_details_screen.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:space_app/core/theming/assets.dart';
-import 'package:space_app/core/theming/colors.dart';
-import 'package:space_app/core/theming/text_styles.dart';
-import 'package:space_app/core/widgets/carousel_slider_builder.dart';
-import 'package:space_app/core/widgets/smooth_indicator.dart';
+import 'package:space_app/core/widgets/details_screen_body.dart';
 import 'package:space_app/core/widgets/title_of_item_details.dart';
 
 class RocketDetailsScreen extends StatefulWidget {
@@ -47,65 +43,3 @@ class _RocketDetailsScreenState extends State<RocketDetailsScreen> {
   }
 }
 
-class DetailsScreenBody extends StatefulWidget {
-  double currentPage;
-  final String description;
-  final List<String> images;
-  final int index;
-
-  DetailsScreenBody(
-      {super.key,
-      required this.currentPage,
-      required this.description,
-      required this.index,
-      required this.images});
-
-  @override
-  State<DetailsScreenBody> createState() => _DetailsScreenBodyState();
-}
-
-class _DetailsScreenBodyState extends State<DetailsScreenBody> {
-  @override
-  Widget build(BuildContext context) {
-    return Expanded(
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              CarouselSliderBuilder(
-                onPageChanged: (page) {
-                  setState(() {
-                    widget.currentPage = page;
-                  });
-                },
-                image: widget.images[widget.currentPage.toInt()],
-                imagesCount: widget.images.length,
-              ),
-              CustomSmoothIndicator(
-                currentPage: widget.currentPage,
-                boardLength: widget.images.length,
-              ),
-              SizedBox(height: 45.h),
-              Text(
-                'Description',
-                style: AppTextStyles.fontLighterGray23W600,
-              ),
-              SizedBox(height: 10.h),
-              Padding(
-                padding: const EdgeInsets.only(left: 8.0),
-                child: Text(
-                  widget.description,
-                  style: AppTextStyles.fontWhite18W500.copyWith(
-                    color: AppColors.primaryMediumGrayColor,
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
