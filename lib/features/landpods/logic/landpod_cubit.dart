0396b3e1f5a1d93bf56d9db4bb30c0970c2be72b
samaa <samaasoltan6@gmail.com>
2024-05-03 15:16:38 +0300

    add landpod cubit

---
+++ lib/features/landpods/logic/landpod_cubit.dart
@@ -0,0 +1,20 @@
+import 'package:bloc/bloc.dart';
+import 'package:space_app/features/landpods/data/landpod_repo_impl.dart';
+import 'package:space_app/features/landpods/logic/landpod_state.dart';
+
+class LandpodCubit extends Cubit<LandpodState> {
+  LandpodCubit({required this.landpodRepo}) : super(const LandpodState.initial());
+
+  final LandpodRepoImpl landpodRepo;
+
+  void fetchLandpods() async {
+    emit(const LandpodState.loading());
+    final landpods = await landpodRepo.getAllLandpods();
+    landpods.when(success: (landpods) {
+      print(landpods.toList().toString());
+      emit(LandpodState.loaded(landpods));
+    }, failure: (error) {
+      emit(LandpodState.error(error.errorModel.message ?? ''));
+    });
+  }
+}
