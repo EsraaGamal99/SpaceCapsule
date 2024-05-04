6da6d954f701d6c749b5e8991a57f6c202efcdf3
earaagamal99 <esraa66m@gmail.com>
2024-05-04 15:51:49 +0300

    add rockets to setup get it

--- lib/core/di/dependency_injection.dart
+++ lib/core/di/dependency_injection.dart
@@ -1,15 +1,14 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
-import 'package:hive/hive.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:space_app/core/helpers/constants.dart';
 import 'package:space_app/core/theming/app_theme_cubit/app_theme_cubit.dart';
 import 'package:space_app/features/dragons/data/repo/dragon_repo_impl.dart';
 import 'package:space_app/features/dragons/logic/dragon_cubit.dart';
-
+import 'package:space_app/features/rockets/data/repo/rockets_repo.dart';
+import 'package:space_app/features/rockets/data/repo/rockets_repo_impl.dart';
+import 'package:space_app/features/rockets/logic/rockets_cubit/rockets_cubit.dart';
 import '../../features/dragons/data/repo/dragon_repo.dart';
 import '../networking/api_service/dio_factory.dart';
-
 import '../../features/localization/data/repos/localization_repo.dart';
 import '../../features/localization/logic/localization_cubit.dart';
 
@@ -24,7 +23,7 @@ Future<void> setupGetIt() async{
   final sharedPref = SharedPreferences.getInstance();
   // Dragons repo
   getIt.registerLazySingleton(() => DragonRepo(dio));
-  getIt.registerLazySingleton(() => DragonRepoImpl(dragonRepo: getIt()));;
+  getIt.registerLazySingleton(() => DragonRepoImpl(dragonRepo: getIt()));
 
   // Dragon app_theme_cubit
   getIt.registerFactory(() => DragonCubit(dragonRepo: getIt()));
@@ -35,4 +34,11 @@ Future<void> setupGetIt() async{
   // Localization
   getIt.registerSingleton<LocalizationRepo>(LocalizationRepo());
   getIt.registerSingleton<LocalizationCubit>(LocalizationCubit());
+
+  // Rockets
+  getIt.registerLazySingleton(() => RocketsRepo(dio));
+  getIt.registerLazySingleton(() => RocketsRepoImpl(rocketsRepo: getIt()));
+  getIt.registerFactory(() => RocketsCubit(rocketsRepo: getIt()));
+
+
 }
