2258a8211d6d823437d9262594bb3adca48027f0
earaagamal99 <esraa66m@gmail.com>
2024-05-05 01:04:04 +0300

    create rockets_bloc_builder and integrate the API with rockets screen

--- lib/features/rockets/ui/screens/rockets_screen.dart
+++ lib/features/rockets/ui/screens/rockets_screen.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:space_app/core/routing/routes.dart';
 import 'package:space_app/core/theming/assets.dart';
-import 'package:space_app/features/home_of_items/ui/screens/widgets/list_of_space_items.dart';
 import 'package:space_app/features/home_of_items/ui/screens/widgets/space_item_app_bar.dart';
 import 'package:space_app/features/rockets/logic/rockets_cubit/rockets_cubit.dart';
+import 'package:space_app/features/rockets/ui/screens/widgets/rockets_bloc_builder.dart';
 
 class RocketsScreen extends StatefulWidget {
   const RocketsScreen({super.key});
@@ -14,7 +13,6 @@ class RocketsScreen extends StatefulWidget {
 }
 
 class _RocketsScreenState extends State<RocketsScreen> {
-
   @override
   void initState() {
     super.initState();
@@ -25,32 +23,22 @@ class _RocketsScreenState extends State<RocketsScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Column(
-        children: [
-          const SpaceItemsAppBar(
-            screenTitle: 'Rockets',
-          ),
-          ListOfSpaceItems(
-            itemsTitle: cardName,
-            itemsImage: imageName,
-           navigateTo: (){
-             Navigator.pushNamed(context, Routes.rocketDetailsScreen);
-           },
-          ),
-        ],
+      body: Container(
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+          image: AssetImage(AppAssets.rocketsBackground),
+          //fit: BoxFit.cover,
+        )),
+        child: const Column(
+          children: [
+            SpaceItemsAppBar(
+              screenTitle: 'Rockets',
+            ),
+            RocketsBlocBuilder(),
+          ],
+        ),
       ),
     );
   }
 }
 
-//TODO: replace it with real data when connected to api "the name and images of rockets"
-List<String> cardName = [
-  'Rocket_1',
-  'Rocket_2',
-  'Rocket_3',
-];
-List<String> imageName = [
-  AppAssets.rockets,
-  AppAssets.galaxy,
-  AppAssets.insightfulImage,
-];
---
+++ lib/features/rockets/ui/screens/widgets/rockets_bloc_builder.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:space_app/core/routing/routes.dart';
+import 'package:space_app/core/widgets/loading_widgets/small_loading_widget.dart';
+import 'package:space_app/features/home/ui/screens/widgets/pick_card_widget.dart';
+import 'package:space_app/features/rockets/logic/rockets_cubit/rockets_cubit.dart';
+import 'package:space_app/features/rockets/logic/rockets_cubit/rockets_state.dart';
+
+class RocketsBlocBuilder extends StatelessWidget {
+  const RocketsBlocBuilder({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<RocketsCubit, RocketsState>(
+      builder: (context, state) {
+        if (state is Success) {
+          debugPrint('***************************${state.data.length}');
+          return Expanded(
+            child: Padding(
+              padding: EdgeInsets.only(
+                  bottom: 15.0.w, left: 20.w, right: 20.w, top: 15.h),
+              child: ListView.separated(
+                  itemBuilder: (context, index) {
+                    return PickCardWidget(
+                      cardName: state.data[index].name,
+                      imageName: state.data[index].flickrImages[1],
+                      isToDetailsScreen: true,
+                      onPressed: () {
+                        Navigator.pushNamed(
+                            context, Routes.rocketDetailsScreen);
+                      },
+                    );
+                  },
+                  separatorBuilder: (context, index) => SizedBox(height: 30.h),
+                  itemCount: state.data.length),
+            ),
+          );
+        } else if (state is Loading) {
+          return Column(
+            children: [
+              SizedBox(height: MediaQuery.of(context).size.height * 0.2),
+              const SmallLoadingWidget(),
+            ],
+          );
+        } else {
+          return Text('cubit.state.error.toString()');
+        }
+      },
+    );
+  }
+}
