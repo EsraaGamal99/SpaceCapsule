78012714e684544621b17b016159eb6ee383995d
earaagamal99 <esraa66m@gmail.com>
2024-05-05 06:11:22 +0300

    refactor home screen

--- lib/features/home/ui/screens/widgets/pick_card_widget.dart
+++ lib/features/home/ui/screens/widgets/pick_card_widget.dart
@@ -8,19 +8,17 @@ import 'package:space_app/core/helpers/extenstions.dart';
 import '../../../../../core/theming/text_styles.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 
-
 class PickCardWidget extends StatelessWidget {
   final String cardName;
   final String imageName;
-  final bool? isToDetailsScreen;
+  final bool isToDetailsScreen;
   final void Function()? onPressed;
 
-  const PickCardWidget(
-      {super.key,
-      required this.cardName,
-      required this.imageName,
-      this.isToDetailsScreen,
-      this.onPressed});
+  const PickCardWidget({super.key,
+    required this.cardName,
+    required this.imageName,
+    required this.isToDetailsScreen,
+    this.onPressed});
 
   @override
   Widget build(BuildContext context) {
@@ -34,11 +32,16 @@ class PickCardWidget extends StatelessWidget {
         children: [
           Container(
             decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: CachedNetworkImageProvider(
-                    imageName,
-                  ),
-                  fit: BoxFit.cover),
+              image: isToDetailsScreen ? DecorationImage(
+                image:
+                CachedNetworkImageProvider(imageName),
+                fit: BoxFit.cover,
+              ):
+              DecorationImage(
+                image:
+                AssetImage(imageName),
+                fit: BoxFit.cover,
+              ),
               borderRadius: BorderRadius.circular(20.h),
             ),
             child: Container(
@@ -62,7 +65,10 @@ class PickCardWidget extends StatelessWidget {
             bottom: 10.h,
             child: Container(
               padding: EdgeInsets.symmetric(horizontal: 22.w),
-              width: MediaQuery.of(context).size.width - 60.w,
+              width: MediaQuery
+                  .of(context)
+                  .size
+                  .width - 60.w,
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
@@ -74,22 +80,25 @@ class PickCardWidget extends StatelessWidget {
                         context.translate.adventurerTextKey,
                         style: AppTextStyles.fontWhite15W500,
                       ),
-                      Text(cardName,style: AppTextStyles.fontWhite33W600,),
+                      Text(
+                        cardName,
+                        style: AppTextStyles.fontWhite33W600,
+                      ),
                     ],
                   ),
                   MaterialButton(
                     onPressed: isToDetailsScreen == true
                         ? onPressed
                         : () {
-                            cardName == 'Rockets'
-                                ? Navigator.pushNamed(
-                                    context, Routes.rocketsScreen)
-                                : (cardName == 'Dragons')
-                                    ? Navigator.pushNamed(
-                                        context, Routes.dragonScreen)
-                                    : Navigator.pushNamed(
-                                        context, Routes.landPodsScreen);
-                          },
+                      cardName == 'Rockets'
+                          ? Navigator.pushNamed(
+                          context, Routes.rocketsScreen)
+                          : (cardName == 'Dragons')
+                          ? Navigator.pushNamed(
+                          context, Routes.dragonScreen)
+                          : Navigator.pushNamed(
+                          context, Routes.landPodsScreen);
+                    },
                     color: AppColors.primaryMediumGrayColor.withOpacity(0.4),
                     padding: EdgeInsets.all(6.h),
                     height: 54.h,
