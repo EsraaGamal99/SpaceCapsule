a101f5a38cfe6a0d38e9a1b28eda30ba8828de22
earaagamal99 <esraa66m@gmail.com>
2024-05-06 00:49:34 +0300

    make the pick card widget navigate

--- lib/features/home/ui/screens/widgets/pick_card_widget.dart
+++ lib/features/home/ui/screens/widgets/pick_card_widget.dart
@@ -18,113 +18,123 @@ class PickCardWidget extends StatelessWidget {
   final bool isToDetailsScreen;
   final void Function()? onPressed;
 
-
-  const PickCardWidget({super.key,
-    required this.cardName,
-    this.locality,
-    required this.imageName,
-    required this.isToDetailsScreen,
-    this.onPressed});
+  const PickCardWidget(
+      {super.key,
+      required this.cardName,
+      this.locality,
+      required this.imageName,
+      required this.isToDetailsScreen,
+      this.onPressed});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 370.w,
-      height: 196.h,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20.h),
-      ),
-      child: Stack(
-        children: [
-          Container(
-            decoration: BoxDecoration(
-
-              image: isToDetailsScreen ? DecorationImage(
-                image:
-                CachedNetworkImageProvider(imageName),
-                fit: BoxFit.cover,
-              ):
-              DecorationImage(
-                image:
-                AssetImage(imageName),
-                fit: BoxFit.cover,
-              ),
-              borderRadius: BorderRadius.circular(20.h),
-            ),
-            child: Container(
+    return GestureDetector(
+      onTap: isToDetailsScreen == true
+          ? onPressed
+          : () {
+              cardName == 'Rockets'
+                  ? Navigator.pushNamed(context, Routes.rocketsScreen)
+                  : (cardName == 'Dragons')
+                      ? Navigator.pushNamed(context, Routes.dragonScreen)
+                      : Navigator.pushNamed(context, Routes.landPodsScreen);
+            },
+      child: Container(
+        width: 370.w,
+        height: 196.h,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20.h),
+        ),
+        child: Stack(
+          children: [
+            Container(
               decoration: BoxDecoration(
+                image: isToDetailsScreen
+                    ? DecorationImage(
+                        image: CachedNetworkImageProvider(imageName),
+                        fit: BoxFit.cover,
+                      )
+                    : DecorationImage(
+                        image: AssetImage(imageName),
+                        fit: BoxFit.cover,
+                      ),
                 borderRadius: BorderRadius.circular(20.h),
-                gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    colors: [
-                      AppColors.primaryWhiteColor.withOpacity(0.1),
-                      AppColors.primaryBlackColor.withOpacity(0.8),
-                    ],
-                    stops: const [
-                      0.0,
-                      1.9
-                    ]),
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20.h),
+                  gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                        AppColors.primaryWhiteColor.withOpacity(0.1),
+                        AppColors.primaryBlackColor.withOpacity(0.8),
+                      ],
+                      stops: const [
+                        0.0,
+                        1.9
+                      ]),
+                ),
               ),
             ),
-          ),
-          Positioned(
-            bottom: 10.h,
-            child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 22.w),
-              width: MediaQuery
-                  .of(context)
-                  .size
-                  .width - 60.w,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Text(
-                        locality ?? context.translate.adventurerTextKey,
-                        style: AppTextStyles.fontWhite15W500,
-                      ),
-                      Text(
-                        cardName,
-                        style: AppTextStyles.fontWhite33W600,
-                      ),
-                    ],
-                  ),
-                  MaterialButton(
-                    onPressed: isToDetailsScreen == true
-                        ? onPressed
-                        : () {
-                      cardName == 'Rockets'
-                          ? Navigator.pushNamed(
-                          context, Routes.rocketsScreen)
-                          : (cardName == 'Dragons')
-                          ? Navigator.pushNamed(
-                          context, Routes.dragonScreen)
-                          : Navigator.pushNamed(
-                          context, Routes.landPodsScreen);
-                    },
-                    color: AppColors.primaryMediumGrayColor.withOpacity(0.4),
-                    padding: EdgeInsets.all(6.h),
-                    height: 54.h,
-                    minWidth: 59.w,
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(18.h)),
-                    child: RotatedBox(
-                      quarterTurns: BlocProvider.of<LocalizationCubit>(context).isArabic() ? 2 : 0,
-                      child: SvgPicture.asset(
-                        AppAssets.forwardArrowIcon,
-                        color: Colors.white,
+            Positioned(
+              bottom: 10.h,
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 22.w),
+                width: MediaQuery.of(context).size.width - 60.w,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          locality ?? context.translate.adventurerTextKey,
+                          style: AppTextStyles.fontWhite15W500,
+                        ),
+                        Text(
+                          cardName,
+                          style: AppTextStyles.fontWhite33W600,
+                        ),
+                      ],
+                    ),
+                    MaterialButton(
+                      onPressed: isToDetailsScreen == true
+                          ? onPressed
+                          : () {
+                              cardName == 'Rockets'
+                                  ? Navigator.pushNamed(
+                                      context, Routes.rocketsScreen)
+                                  : (cardName == 'Dragons')
+                                      ? Navigator.pushNamed(
+                                          context, Routes.dragonScreen)
+                                      : Navigator.pushNamed(
+                                          context, Routes.landPodsScreen);
+                            },
+                      color: AppColors.primaryMediumGrayColor.withOpacity(0.4),
+                      padding: EdgeInsets.all(6.h),
+                      height: 54.h,
+                      minWidth: 59.w,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18.h)),
+                      child: RotatedBox(
+                        quarterTurns:
+                            BlocProvider.of<LocalizationCubit>(context)
+                                    .isArabic()
+                                ? 2
+                                : 0,
+                        child: SvgPicture.asset(
+                          AppAssets.forwardArrowIcon,
+                          color: Colors.white,
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
