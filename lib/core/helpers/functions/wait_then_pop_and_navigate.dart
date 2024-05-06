53fb651619a6a3aebb664844ce266c0523809d9b
samaa <samaasoltan6@gmail.com>
2024-05-06 22:42:19 +0300

    fix navigate to lon in

--- lib/core/helpers/functions/wait_then_pop_and_navigate.dart
+++ lib/core/helpers/functions/wait_then_pop_and_navigate.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
-Future<void> waitThenPopAndNavigate(BuildContext context,String? routName ) async {
+Future<void> waitThenPopAndNavigate(BuildContext context,String? routName,
+    {bool? showBackButton = false}) async {
   await Future.delayed(const Duration(seconds: 2));
   if (!context.mounted) return;
   Navigator.pop(context);
-  (routName != null )?Navigator.pushReplacementNamed(context, routName): null;
+  (routName != null )?Navigator.pushReplacementNamed(context, routName, arguments: showBackButton): null;
 }
