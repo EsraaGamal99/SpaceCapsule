95d6d1e4af7ad5a0fad09d124951d2ba71c94acc
samaa <samaasoltan6@gmail.com>
2024-05-07 19:31:07 +0300

    lock icon color delete unused imports fix scroll in home screen

--- lib/features/dragons/ui/screens/dragon_screen.dart
+++ lib/features/dragons/ui/screens/dragon_screen.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:space_app/core/routing/routes.dart';
 import 'package:space_app/core/theming/assets.dart';
 import 'package:space_app/features/dragons/ui/widgets/dragons_builder.dart';
-import 'package:space_app/features/home_of_items/ui/screens/widgets/list_of_space_items.dart';
 import 'package:space_app/features/home_of_items/ui/screens/widgets/space_item_app_bar.dart';
 
 import '../../logic/dragon_cubit.dart';
